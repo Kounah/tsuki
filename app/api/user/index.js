d77629c46780e5db01ba7df686c29960150c0f92
Kounah <kounalyn@gmail.com>
2019-07-02 13:25:17 +0200

    added permissions to index

--- app/api/user/index.js
+++ app/api/user/index.js
@@ -1,9 +1,12 @@
 const api = require('./api');
 const model = require('./model');
+const permissions = require('./permissions');
 const router = require('./router');
+
 
 module.exports = {
   api,
   model,
+  permissions,
   router
 };
