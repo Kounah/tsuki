c4cef7037172a7d01827b1313b2526a3740aac3a
Kounah <kounalyn@gmail.com>
2019-07-22 12:41:39 +0200

    config file rework, now including JSDoc @type definitions for each setting function setting now uses only one params parameter

--- app/config.js
+++ app/config.js
@@ -18,19 +18,26 @@ function jsonpath(o, ...path) {
 
 /**
  * handles the settings
- * @param {String} name - setting name\
+ * @param {Object} params
+ * @param {String} params.key - setting name\
  * no property name may include `.` since the name consists of a dot-separated
  * list of property names
- * @param {*} defaultvalue - the default value of the setting
- * @param {(val:*) => *} vcb - value callback\
+ * @param {*} params.def - the default value of the setting
+ * @param {(val:*) => *} params.mod - value modifier callback\
  * a callback executed on `val`
  */
-function setting(name, defaultvalue, vcb) {
-  let val =  process.env[`tsuki.${name}`] ||
-    jsonpath(uconfig, ...name.split('.')) ||
-    defaultvalue;
-  if(typeof vcb === 'function')
-    val = vcb(val);
+function setting(params) {
+  if(typeof params !== 'object' || params === null)
+    throw new TypeError('\'params\' was not an object');
+
+  if(typeof params.key !== 'string' || !params.key)
+    throw new TypeError('\'params.key\' was not a string or falsy');
+
+  let val =  process.env[`tsuki.${params.key}`] ||
+    jsonpath(uconfig, ...params.key.split('.')) ||
+    params.def;
+  if(typeof params.mod === 'function')
+    val = params.mod(val);
   return val;
 }
 
@@ -42,88 +49,253 @@ function ifEmptyString(returnvalue) {
 
 // configures the connection to the database
 module.exports.database = {
-  host:       setting('database.host',      '127.0.0.1'),
-  port:       setting('database.port',      27017, (val) => Number(val)),
-  name:       setting('database.name',      'tsuki'),
-  username:   setting('database.username',  '', ifEmptyString(undefined)),
-  password:   setting('database.password',  '', ifEmptyString(undefined))
+  /**@type {String} */
+  host: setting({
+    key: 'database.host',
+    def: '127.0.0.1'
+  }),
+  /**@type {Number} */
+  port: setting({
+    key: 'database.port',
+    def: 27017,
+    mod: val => Number(val)
+  }),
+  /**@type {String} */
+  name: setting({
+    key: 'database.name',
+    def: 'tsuki'
+  }),
+  /**@type {String} */
+  username: setting({
+    key: 'database.username',
+    def: '',
+    mod: ifEmptyString(undefined)
+  }),
+  /**@type {String} */
+  password: setting({
+    key: 'database.password',
+    def: '',
+    mod: ifEmptyString(undefined)
+  })
 };
 
 // configures the server
 module.exports.server = {
-  port: setting('server.port', 5000, (val) => Number(val)),
-  'request-delay': setting('server.request-delay', 0, (val) => Number(val)),
-  'response-delay': setting('server.request-delay', 0, (val) => Number(val)),
-  session: {
-    secret: setting('server.session.secret', ((os, crypto, package) => {
-      return crypto.createHash('sha512').update([
-        package['name'],
-        package['version'],
-        os.userInfo().username,
-        os.hostname(),
-        os.platform(),
-        os.arch(),
-        os.release(),
-        os.type()
-      ].join(':')).digest('hex');
-    })(require('os'), require('crypto'), require('../package.json'))),
-    resave: setting('server.session.resave', false, val => Boolean(val)),
-    'save-uninitialized': setting('server.session.save-uninitialized', true, val => Boolean(val)),
-    cookie: {
-      secure: setting('server.session.cookie.secure', true, val => Boolean(val)),
-      'max-age': setting('server.session.cookie.max-age', null, val => eval(val))
+  /**@type {Number} */
+  'port': setting({
+    key: 'server.port',
+    def: 5000,
+    mod: val => Number(val)
+  }),
+  /**@type {Number} */
+  'request-delay': setting({
+    key: 'server.request-delay',
+    def: 0,
+    mod: val => Number(val)
+  }),
+  'response-delay': setting({
+    key: 'server.request-delay',
+    def: 0,
+    mod: val => Number(val)
+  }),
+  'session': {
+    /**@type {String} */
+    'secret': setting({
+      key: 'server.session.secret',
+      // creating the secret for the system it runs on
+      def: ((os, crypto, package) => {
+        return crypto.createHash('sha512').update([
+          package['name'],
+          package['version'],
+          os.userInfo().username,
+          os.hostname(),
+          os.platform(),
+          os.arch(),
+          os.release(),
+          os.type()
+        ].join(':')).digest('hex');
+      })(require('os'), require('crypto'), require('../package.json'))
+    }),
+    /**@type {Boolean} */
+    'resave': setting({
+      key: 'server.session.resave',
+      def: false,
+      mod: val => Boolean(val)
+    }),
+    /**@type {Boolean} */
+    'save-uninitialized': setting({
+      key: 'server.session.save-uninitialized',
+      def: true,
+      mod: val => Boolean(val)
+    }),
+    'cookie': {
+      /**@type {Boolean} */
+      'secure': setting({
+        key: 'server.session.cookie.secure',
+        def: true,
+        mod: val => Boolean(val)
+      }),
+      /**@type {Number} */
+      'max-age': setting({
+        key: 'server.session.cookie.max-age',
+        def: null,
+        mod: val => eval(val)
+      })
     }
   },
-  security: {
-    'print-session': setting('server.security.print-session', false, val => Boolean(val)),
-    'print-session-json': setting('server.security.print-session-json', false, val => Boolean(val)),
+  'security': {
+    /**@type {Boolean} */
+    'print-session': setting({
+      key: 'server.security.print-session',
+      def: false,
+      mod: val => Boolean(val)
+    }),
+    /**@type {Boolean} */
+    'print-session-json': setting({
+      key: 'server.security.print-session-json',
+      def: false,
+      mod: val => Boolean(val)
+    }),
   }
 };
 
 // configures credentials
 module.exports.credentials = {
   admin: {
-    username: setting('credentials.admin.username', 'admin'),
-    password: setting('credentials.admin.password', '$admin0!')
+    /**@type {String} */
+    username: setting({
+      key: 'credentials.admin.username',
+      def: 'admin'
+    }),
+    /**@type {String} */
+    password: setting({
+      key: 'credentials.admin.password',
+      def: '$admin0!'
+    })
   },
-  _rules: {
-    user: {
-    }
-  }
 };
 
 module.exports.apis = {
-  user: setting('apis.user', true)
+  /**@type {Boolean} */
+  user: setting({
+    key: 'apis.user',
+    def: true,
+    mod: val => Boolean(val)
+  })
 };
 
 module.exports.api = {
   v1: {
-    prefix: setting('api.v1.prefix', '/api/v1'),
+    /**@type {String} */
+    prefix: setting({
+      key: 'api.v1.prefix',
+      def: '/api/v1'}),
     user: {
-      create: setting('api.v1.user.create', true),
-      login: setting('api.v1.user.login', true),
-      register: setting('api.v1.user.register', true)
+      /**@type {Boolean} */
+      create: setting({
+        key: 'api.v1.user.create',
+        def: true,
+        mod: val => Boolean(val)}),
+      /**@type {Boolean} */
+      login: setting({
+        key: 'api.v1.user.login',
+        def: true,
+        mod: val => Boolean(val)}),
+      /**@type {Boolean} */
+      register: setting({
+        key: 'api.v1.user.register',
+        def: true,
+        mod: val => Boolean(val)}),
+
+      validation: {
+        login: {
+          /**@type {RegExp} */
+          'pattern': setting({
+            key: 'api.v1.user.validation.login.pattern',
+            def: /^[a-zA-Z]+[-_a-zA-Z0-9]*$/gm,
+            mod: val => RegExp(val)
+          }),
+          /**@type {Number} */
+          'min-length': setting({
+            key: 'api.v1.user.validation.login.min-length',
+            def: 4,
+            mod: val => Number(val)
+          }),
+          /**@type {Number} */
+          'max-length': setting({
+            key: 'api.v1.user.validation.login.max-length',
+            def: 20,
+            mod: val => Number(val)
+          })
+        },
+        password: {
+          /**@type {RegExp} */
+          'pattern': setting({
+            key: 'api.v1.user.validation.password.pattern',
+            def: /^.*$/gm,
+            mod: val => RegExp(val)
+          }),
+          /**@type {Number} */
+          'min-length': setting({
+            key: 'api.v1.user.validation.password.min-length',
+            def: 8,
+            mod: val => Number(val)
+          }),
+          /**@type {Number} */
+          'max-length': setting({
+            key: 'api.v1.user.validation.password.max-length',
+            def: 512,
+            mod: val => Number(val)
+          })
+        }
+      }
     }
   }
 };
 
 module.exports.nunjucks = {
-  autoescape: setting('nunjucks.autoescape', true),
-  'no-cache': setting('nunjucks.no-cache', false),
+  /**@type {Boolean} */
+  'autoescape': setting({
+    key: 'nunjucks.autoescape',
+    def: true,
+    mod: val => Boolean(val)
+  }),
+  /**@type {Boolean} */
+  'no-cache': setting({
+    key: 'nunjucks.no-cache',
+    def: false,
+    mod: val => Boolean(val)
+  }),
 };
 
 module.exports.pages = {
-  index: setting('pages.index', true),
-  login: setting('pages.login', true),
-  register: setting('pages.register', true)
+  /**@type {Boolean} */
+  index: setting({
+    key: 'pages.index',
+    def: true,
+    mod: val => Boolean(val)
+  }),
+  /**@type {Boolean} */
+  login: setting({
+    key: 'pages.login',
+    def: true,
+    mod: val => Boolean(val)
+  }),
+  /**@type {Boolean} */
+  register: setting({
+    key: 'pages.register',
+    def: true,
+    mod: val => Boolean(val)
+  })
 };
 
 module.exports.templates = {
-  'use-no-cache': setting('templates.use-no-cache', true),
-  validation: {
-    'user-min-pwd-length': setting('templates.validation.user-min-pwd-length', 8, val => Number(val))
-  }
-
+  /**@type {Boolean} */
+  'use-no-cache': setting({
+    key: 'templates.use-no-cache',
+    def: true,
+    mod: val => Boolean(val)
+  }),
 };
 console.log('config:', module.exports);
 
